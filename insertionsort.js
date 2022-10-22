96b73aa889648505553d419d39c210c3ce14a9e5
Bhavik Prajapati <109121443+Bhavik-prajapati@users.noreply.github.com>
2022-10-22 17:27:08 +0530

    Create insertionsort.js

---
+++ insertionsort.js
@@ -0,0 +1,11 @@
+function insertionsort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+    let key = arr[i];
+    while (arr[j] > key && j >= 0) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+    arr[j + 1] = key;
+  }
+}
